2961a79d7e96249a13af27714813294b97faa149
sukhpreetsing04 <sukhpreetsingh19289@gmail.com>
2025-09-25 15:59:17 +0530

    ScrollView Component

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
-import { View, Text, Image, ImageBackground } from "react-native";
+import { View, Text, Image, ScrollView } from "react-native";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      {/* <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      <Image
-        source={{ uri: "https://picsum.photos/200"}}
-        style={{ width: 300, height: 300 }}
-      /> */}
-      <ImageBackground source={logoImg} style={{flex: 1}}>
-        <Text>IMAGE TEXT</Text>
-      </ImageBackground>
+      <ScrollView>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Text>
+          lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Text>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </ScrollView>
     </View>
   );
 }
