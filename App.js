9633051723a9f0dfbd768f813188866ed3910c96
sukhpreetsing04 <sukhpreetsingh19289@gmail.com>
2025-09-25 15:49:06 +0530

    Image Components and different methods to render different types images.

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Text>
-        <Text style={{ color: "white" }}>Hello</Text> World
-      </Text>
+      {/* <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/200"}}
+        style={{ width: 300, height: 300 }}
+      /> */}
+      <ImageBackground source={logoImg} style={{flex: 1}}>
+        <Text>IMAGE TEXT</Text>
+      </ImageBackground>
     </View>
   );
 }
